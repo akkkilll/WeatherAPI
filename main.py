f3da1d0b9807b10b1e4b324e8576f9dc4920f97e
Akhil <bakhil696@gmail.com>
2023-01-10 14:22:03 +0530

    Return temperature for station and date

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename)
-    temperature = 32
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    
+    temperature = df.loc[df['    DATE']==date]['   TG'].squeeze()/10
+    
+    # temperature = 32
     return {
         "station": station,
         "date": date,
